0b7613cae836b23c034057f36ed51c8fdc05b0ae
Voling <dethanvo@gmail.com>
2024-10-20 08:53:14 +0000

    Interact with model, fix cors, create sessions

--- server/src/routes/upload/fileUpload.ts
+++ server/src/routes/upload/fileUpload.ts
@@ -12,21 +12,21 @@ router.post('/upload/getPresignedUrl', async (req, res) => {
     res.status(500).json({ error: 'Failed to generate presigned URL' });
   }
 });
-router.post('/upload/notifyComplete', async (req, res): Promise<void> => {
-  const { fileName } = req.body;
-  
-  if (!fileName) {
-    res.status(400).send('File name is required');
+router.post('/upload/notifyModel', async (req, res): Promise<void> => {
+  const { fileNames } = req.body;
+
+  if (!fileNames || !Array.isArray(fileNames) || fileNames.length === 0) {
+    res.status(400).send('Files are required');
     return;
   }
 
   try {
-    const result = await handleUserS3Upload(fileName); 
+    const result = await handleUserS3Upload(fileNames); 
     res.status(200).json(result);  //forward the result S3 URL to the client
   } catch (error) {
     if (error instanceof Error)
       res.status(500).json({ error: error.message });
-    console.log(error)
+    console.log("Error during model processing", error)
   }
 });
 export default router;
