9852fb48ded434d62090728123b104dc94f3d7d3
Evan Servito <78441979+evanpservito@users.noreply.github.com>
2024-10-20 01:59:16 -0700

    update front page styling

--- client/components/Team.tsx
+++ client/components/Team.tsx
@@ -5,7 +5,7 @@ import { team } from "../app/data/team";
 const Team = () => {
   return (
     <div className="flex flex-col items-center justify-center p-16 min-h-svh">
-      <div className="text-4xl font-semibold animate-fadeup">Team</div>
+      <div className="text-4xl font-semibold animate-fadeup">Meet The Team</div>
       <p className="p-16 font-medium text-xl">
         A group of students from the University of California, Irvine.
       </p>
