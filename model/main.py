0999a822f11e35ad70f1d1260c2fc66579028f21
Zion Mateo <mateoz@uci.edu>
2024-10-20 00:14:53 -0700

    this is my world now

--- model/main.py
+++ model/main.py
@@ -11,10 +11,14 @@ load_dotenv()
 
 app = FastAPI()
 
+print("AWS secret key", os.getenv('AWS_ACCESS_KEY_ID'))
+print("AWS secret access", os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+
 s3 = boto3.client(
     's3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key= os.getenv('AWS_SECRET_KEY'),
+    aws_secret_access_key= os.getenv('AWS_SECRET_ACCESS_KEY'),
     region_name=os.getenv('AWS_REGION')
 )
 
